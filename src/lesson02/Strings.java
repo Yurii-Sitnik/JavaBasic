56f3a8a3b0a7cbb0950bd0e6e6e48ebbdc1a58e1
Sytnyk Yurii <yoursit1980@gmail.com>
2022-07-09 22:46:27 +0300

    My commit new Branch

--- src/lesson02/Strings.java
+++ src/lesson02/Strings.java
@@ -1,7 +1,7 @@
 package lesson02;
 public class Strings {
     public static void main(String[] args) {
-        int x = 5;
+        int x = 6;
         String s = "Hello";
         String spase = " ";
         String name = "Bob";
