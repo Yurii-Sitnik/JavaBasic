54295bf6bb4127dc94eddd743a7954e071cbf51e
Sytnyk Yurii <yoursit1980@gmail.com>
2022-07-10 12:26:23 +0300

    Add new class Bet

---
+++ src/lesson12/Bet.java
@@ -0,0 +1,48 @@
+package lesson12;
+
+import java.util.Scanner;
+
+class Bet {
+    private int value;
+    private double risk;
+
+    public Bet(int value, double risk) {
+        this.value = value;
+        this.risk = risk;
+    }
+
+    public double getRisk() {
+        return risk;
+    }
+
+    public void setRisk(double risk) {
+        this.risk = risk;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Введите значение ставки и риск через пробел");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        String[] bet = string.split(" ");
+        int value = Integer.parseInt(bet[0]);
+        double risk = Double.parseDouble(bet[1]);
+
+        Bet bet1 = new Bet(value, risk);
+        bet1.setValue(value);
+        bet1.setRisk(risk);
+
+        int valueToReturn = bet1.getValue();
+        double riskToReturn = bet1.getRisk();
+
+        System.out.println("Ставка " + valueToReturn);
+        System.out.println("Риск " + riskToReturn);
+    }
+}
