ea2cb4a005b00b10785864c10c94976ba69645e8
Sytnyk Yurii <yoursit1980@gmail.com>
2022-07-14 23:36:52 +0300

    Add file Alishev 25,26

---
+++ src/alishev25/Alishew25.java
@@ -0,0 +1,17 @@
+package alishev25;
+
+public class Alishew25 {
+    public static void main(String[] args) {
+      // Alishev_25.Animal animal = new Alishev_25.Animal();
+      // animal.eat();
+      // animal.sleep();
+        Dog dog = new Dog();
+      // dog.eat();
+      // dog.sleep();
+      // dog.bark();
+        dog.eat();
+        dog.showName();
+
+    }
+}
+
---
+++ src/alishev25/Dog.java
@@ -0,0 +1,20 @@
+package alishev25;
+
+public class Dog extends Animal {
+    public void eat() {
+        System.out.println("Alishev_25.Dog is eating");
+
+    }
+
+    public void bark() {
+        System.out.println("I am barking");
+    }
+    public void showName(){
+        System.out.println(name);
+    }
+}
+
+
+
+
+
