ee36bff037679f01df9da9b4beb2eba10ef2d67a
Sytnyk Yurii <yoursit1980@gmail.com>
2022-11-27 21:56:37 +0200

    Commit

--- src/lesson10/RepeatingElements.java
+++ src/lesson10/RepeatingElements.java
@@ -14,7 +14,7 @@ public class RepeatingElements {
         for (int i = 0; i < arr.length-1; i++) {
            for (int j = i + 1; j < arr.length; j++) {
                if (arr[i] == arr[j]) {
-                  arr = ArrayUtils.remove(arr, j); //импортировал метод remove, который удал€ет повтор€ющиес€ элементы
+                  //arr = ArrayUtils.remove(arr, j); //импортировал метод remove, который удал€ет повтор€ющиес€ элементы
                    }
             }
         }
---
+++ src/traning/Car.java
@@ -0,0 +1,13 @@
+package traning;
+
+public class Car {
+   final private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void printCarName(){
+          System.out.println(name);
+      }
+}
